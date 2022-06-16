3278a9837e7e13fb7fa64d51c5dd2a872605696d
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-16 21:49:18 +0200

    Modificato titolo presente in home e rimosso rotte superflue.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container flex_cent flex-column">
 
-    <h1>Questo testo è presente solo nel file home</h1>
+    <h1>Laravel Base Crud</h1>
     <!-- Link per comics -->
     <p>Verifica la lista dei fumetti qui : <a href="{{route('comics.index')}}"> INDEX </a> </p>
 
